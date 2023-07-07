fe1fc417b452b3cb54dc75980a44ac82b4f8a797
matéo! <6matbub@gmail.com>
2023-07-06 21:00:12 -0700

    Modify Form Fields

--- components/ProfileSettingsPersonalInformationForm/ProfileSettingsPersonalInformationForm.jsx
+++ components/ProfileSettingsPersonalInformationForm/ProfileSettingsPersonalInformationForm.jsx
@@ -59,20 +59,6 @@ const ProfileSettingsPersonalInformationForm = () => {
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  {...register("email")}
-                  type="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                 Country
               </label>
--- components/ProfileSettingsProfileForm/ProfileSettingsProfileForm.jsx
+++ components/ProfileSettingsProfileForm/ProfileSettingsProfileForm.jsx
@@ -33,17 +33,17 @@ const ProfileSettingsProfileForm = () => {
         <div className="px-4 py-6 sm:p-8">
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
-                Website
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Username
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">http://</span>
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">@</span>
                   <input
-                    {...register("website")}
+                    {...register("username")}
                     type="text"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="www.example.com"
+                    placeholder="slick-rick"
                   />
                 </div>
               </div>
