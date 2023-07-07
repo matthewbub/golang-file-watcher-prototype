0b1a8dcb0c3e7de2f4e4d24722ac109e40848c79
matéo! <6matbub@gmail.com>
2023-07-06 20:07:32 -0700

    Connect ProfileSettingsProfileForm to react-hook-form

--- components/ProfileSettingsProfileForm/ProfileSettingsProfileForm.jsx
+++ components/ProfileSettingsProfileForm/ProfileSettingsProfileForm.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 /**
   Designed to sit within the following layout:
   
@@ -8,9 +10,16 @@
   </div>
  */
 import { Fragment } from "react"
+import { useForm } from "react-hook-form";
 import { UserCircleIcon, PhotoIcon } from "@heroicons/react/24/outline";
 
 const ProfileSettingsProfileForm = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  }
+
   return (
     <Fragment>
       <div className="px-4 sm:px-0">
@@ -20,7 +29,7 @@ const ProfileSettingsProfileForm = () => {
         </p>
       </div>
 
-      <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
         <div className="px-4 py-6 sm:p-8">
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
@@ -31,9 +40,8 @@ const ProfileSettingsProfileForm = () => {
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">http://</span>
                   <input
+                    {...register("website")}
                     type="text"
-                    name="website"
-                    id="website"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="www.example.com"
                   />
@@ -47,6 +55,7 @@ const ProfileSettingsProfileForm = () => {
               </label>
               <div className="mt-2">
                 <textarea
+                  {...register("about")}
                   id="about"
                   name="about"
                   rows={3}
@@ -63,12 +72,11 @@ const ProfileSettingsProfileForm = () => {
               </label>
               <div className="mt-2 flex items-center gap-x-3">
                 <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-                <button
-                  type="button"
+                <input
+                  {...register("photo")}
+                  type="file"
                   className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Change
-                </button>
+                />
               </div>
             </div>
 
@@ -85,7 +93,11 @@ const ProfileSettingsProfileForm = () => {
                       className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
                       <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                      <input
+                        {...register("coverPhoto")}
+                        type="file"
+                        className="sr-only"
+                      />
                     </label>
                     <p className="pl-1">or drag and drop</p>
                   </div>
@@ -96,7 +108,9 @@ const ProfileSettingsProfileForm = () => {
           </div>
         </div>
         <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={() => {
+            reset();
+          }}>
             Cancel
           </button>
           <button
