9e34724427ab353020f214b0d92766fc96cc5e37
matéo! <6matbub@gmail.com>
2023-07-06 19:53:54 -0700

    Connect ProfileSettingsPersonalInformationForm to react-hook-form

--- components/ProfileSettingsPersonalInformationForm/ProfileSettingsPersonalInformationForm.jsx
+++ components/ProfileSettingsPersonalInformationForm/ProfileSettingsPersonalInformationForm.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 /**
   Designed to sit within the following layout:
   
@@ -8,8 +10,16 @@
   </div>
  */
 import { Fragment } from "react";
+import { useForm } from "react-hook-form";
 
 const ProfileSettingsPersonalInformationForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+
+    console.log(data);
+  }
+
   return (
     <Fragment>
       <div className="px-4 sm:px-0">
@@ -17,18 +27,17 @@ const ProfileSettingsPersonalInformationForm = () => {
         <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
       </div>
 
-      <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
         <div className="px-4 py-6 sm:p-8">
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
                 First name
               </label>
               <div className="mt-2">
                 <input
+                  {...register("firstName")}
                   type="text"
-                  name="first-name"
-                  id="first-name"
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -36,14 +45,13 @@ const ProfileSettingsPersonalInformationForm = () => {
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
                 Last name
               </label>
               <div className="mt-2">
                 <input
+                  {...register("lastName")}
                   type="text"
-                  name="last-name"
-                  id="last-name"
                   autoComplete="family-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -56,8 +64,7 @@ const ProfileSettingsPersonalInformationForm = () => {
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
+                  {...register("email")}
                   type="email"
                   autoComplete="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -71,8 +78,7 @@ const ProfileSettingsPersonalInformationForm = () => {
               </label>
               <div className="mt-2">
                 <select
-                  id="country"
-                  name="country"
+                  {...register("country")}
                   autoComplete="country-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
@@ -84,14 +90,13 @@ const ProfileSettingsPersonalInformationForm = () => {
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="streetAddress" className="block text-sm font-medium leading-6 text-gray-900">
                 Street address
               </label>
               <div className="mt-2">
                 <input
+                  {...register("streetAddress")}
                   type="text"
-                  name="street-address"
-                  id="street-address"
                   autoComplete="street-address"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -104,9 +109,8 @@ const ProfileSettingsPersonalInformationForm = () => {
               </label>
               <div className="mt-2">
                 <input
+                  {...register("city")}
                   type="text"
-                  name="city"
-                  id="city"
                   autoComplete="address-level2"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -119,9 +123,8 @@ const ProfileSettingsPersonalInformationForm = () => {
               </label>
               <div className="mt-2">
                 <input
+                  {...register("region")}
                   type="text"
-                  name="region"
-                  id="region"
                   autoComplete="address-level1"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -129,14 +132,13 @@ const ProfileSettingsPersonalInformationForm = () => {
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="postalCode" className="block text-sm font-medium leading-6 text-gray-900">
                 ZIP / Postal code
               </label>
               <div className="mt-2">
                 <input
+                  {...register("postalCode")}
                   type="text"
-                  name="postal-code"
-                  id="postal-code"
                   autoComplete="postal-code"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
