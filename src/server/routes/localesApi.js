56b9767cbe5d19d3dc02dfd4af5ed212c0223c6e
matéo! <6matbub@gmail.com>
2023-07-13 12:37:20 -0700

    [UCAN2-42] Locale API Documentation

--- src/server/routes/localesApi.js
+++ src/server/routes/localesApi.js
@@ -1,62 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const {
+  handleGet,
+  handlePost,
+  handlePut,
+  handleDelete,
+  handleGetById
+} = require('../helpers/databaseOperations');
 const { supabase } = require('../supabase.config');
+const table = supabase.from('messages');
 
-router.get("/api/v1/locales", async (_, res) => {
-  const { data, error } = await supabase
-    .from('locales')
-    .select('*')
+router.get("/api/v1/locales", (_, res) => handleGet(res, table));
 
-  if (error) {
-    res.status(500).json({ error: error.message })
-  } else {
-    res.status(200).json({ data })
-  }
-});
-
-router.post('/api/v1/locales', async (req, res) => {
+router.post('/api/v1/locales', (req, res) => {
   const { locale, message, message_key, screen_id } = req.body;
-  const { data, error } = await supabase
-    .from('locales')
-    .insert([{ locale, message, message_key, screen_id }])
-    .single();
-
-  if (error) {
-    res.status(500).json({ error: error.message })
-  } else {
-    res.status(201).json({ data })
-  }
+  handlePost(res, table, [{ locale, message, message_key, screen_id }]);
 });
 
-router.put('/api/v1/locales/:id', async (req, res) => {
+router.put('/api/v1/locales/:id', (req, res) => {
   const { id } = req.params;
   const { locale, message, message_key } = req.body;
-  const { data, error } = await supabase
-    .from('locales')
-    .update({ locale, message, message_key })
-    .eq('id', id)
-    .single();
-
-  if (error) {
-    res.status(500).json({ error: error.message })
-  } else {
-    res.status(200).json({ data })
-  }
+  handlePut(res, table, id, { locale, message, message_key });
 });
 
-router.delete('/api/v1/locales/:id', async (req, res) => {
+router.delete('/api/v1/locales/:id', (req, res) => {
   const { id } = req.params;
-  const { data, error } = await supabase
-    .from('locales')
-    .delete()
-    .eq('id', id)
-    .single();
+  handleDelete(res, table, id);
+});
 
-  if (error) {
-    res.status(500).json({ error: error.message })
-  } else {
-    res.status(200).json({ data })
-  }
+router.get('/api/v1/locales/:id', (req, res) => {
+  const { id } = req.params;
+  handleGetById(res, table, id);
 });
 
 module.exports = router;
