5f19a3c5f5061f612e4acb500f39e9d88028ef22
matéo! <6matbub@gmail.com>
2023-07-16 06:20:50 -0700

    [UCAN2-82] Update typography

--- src/tenants/1993/tailwind.config.js
+++ src/tenants/1993/tailwind.config.js
@@ -22,9 +22,9 @@ module.exports = {
         '4xl': '2.5rem',
       },
       fontFamily: {
-        sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
         display: [
-          ['Mona Sans', ...defaultTheme.fontFamily.sans],
+          ['Inter', ...defaultTheme.fontFamily.sans],
           { fontVariationSettings: '"wdth" 125' },
         ],
       },
