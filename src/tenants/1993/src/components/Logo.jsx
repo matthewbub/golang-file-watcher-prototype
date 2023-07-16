0a0e1dc91efab68fad49d10b8ed785d9ba5f7498
matéo! <6matbub@gmail.com>
2023-07-16 06:18:45 -0700

    [UCAN2-82] Update logo

--- src/tenants/1993/src/components/Logo.jsx
+++ src/tenants/1993/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import { useId } from 'react'
 import clsx from 'clsx'
 import Image from 'next/image'
-import logo from '@/images/logo.svg';
+import logo from '@/images/YucaipaLandscapingLogo.png';
 
 export function Logomark({ invert = false, filled = false, ...props }) {
   let id = useId()
