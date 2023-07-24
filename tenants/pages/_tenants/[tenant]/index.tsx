217e4f4afe0292cdd841a9a7ea10e92e41b61474
matéo! <6matbub@gmail.com>
2023-07-23 19:15:43 -0700

    [UCAN2-129] Install styled-jsx for easier CSS

--- tenants/pages/_tenants/[tenant]/index.tsx
+++ tenants/pages/_tenants/[tenant]/index.tsx
@@ -1,5 +1,3 @@
-import { Fragment, useState } from 'react'
-import clsx from 'clsx'
 import { Header } from '../../../components/__TemplateComponents/Header_NonProfit'
 import { Hero } from '../../../components/__TemplateComponents/Hero_NonProfit'
 import { ThreeCards } from '../../../components/__TemplateComponents/ThreeCards_NonProfit'
