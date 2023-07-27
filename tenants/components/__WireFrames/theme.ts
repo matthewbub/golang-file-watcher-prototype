d5edf24fba35edfbb8ae3e2f92c7f440ef0c534c
matéo! <6matbub@gmail.com>
2023-07-26 19:05:53 -0700

    [UCAN1-129] Hero - Aardvark Variant

---
+++ tenants/components/__WireFrames/theme.ts
@@ -0,0 +1,37 @@
+export const ids = {
+  heroAardvark: 'hero__aardvark',
+}
+export const theme = {
+  primaryColor: '#D98900',
+  primaryTextColor: '#141414',
+  secondaryTextColor: '#333333',
+
+  heroEyebrow__fontWeight: '700',
+  heroTitle__fontWeight: '800',
+  heroSubtitle__fontWeight: '400',
+
+  heroEyebrow__fontSize__mobile: '14px',
+  heroEyebrow__fontSize__tablet: '16px',
+  heroEyebrow__fontSize__desktop: '20px',
+
+  heroTitle__fontSize__mobile: '40px',
+  heroTitle__fontSize__tablet: '50px',
+  heroTitle__fontSize__desktop: '60px',
+
+  heroSubtitle__fontSize__mobile: '16px',
+  heroSubtitle__fontSize__tablet: '16px',
+  heroSubtitle__fontSize__desktop: '20px',
+
+  heroEyebrow__lineHeight__mobile: '20px',
+  heroEyebrow__lineHeight__tablet: '20px',
+  heroEyebrow__lineHeight__desktop: '25px',
+
+  heroTitle__lineHeight__mobile: '45px',
+  heroTitle__lineHeight__tablet: '60px',
+  heroTitle__lineHeight__desktop: '75px',
+
+  heroSubtitle__lineHeight__mobile: '25px',
+  heroSubtitle__lineHeight__tablet: '25px',
+  heroSubtitle__lineHeight__desktop: '30px',
+
+}
--- tenants/pages/home/index.jsx
+++ tenants/pages/home/index.jsx
@@ -1,17 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useEffect } from 'react'
-const inter = Inter({ subsets: ['latin'] })
-import { useForm } from 'react-hook-form'
-import logo from '../../public/light_logo_only.png'
+import { Hero } from '../../components/__WireFrames/Heros/Hero__Aardvark'
 export default function HomePage() {
 
   return (
-    <div className="">
-      <header>
-        <img src={logo} />
-      </header>
-    </div>
-
+    <>
+      <div className="">
+        <Hero />
+      </div>
+    </>
   )
 }
