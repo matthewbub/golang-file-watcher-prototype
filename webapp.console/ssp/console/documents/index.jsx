4435e8032c77cfb4f469b01b24c5bf571b7a42cf
matéo! <6matbub@gmail.com>
2023-07-31 14:49:49 -0700

    Refactor Supabase Query

--- webapp.console/ssp/console/documents/index.jsx
+++ webapp.console/ssp/console/documents/index.jsx
@@ -5,20 +5,16 @@ export const getServerSideProps = async (context) => {
   const token = context.req.cookies.accessToken;
   const decodedToken = jwt.verify(token, process.env.SUPABASE_JWT);
 
-  const { data: user, error: userError } = await supabase
+  const { data, error } = await supabase
     .from('users')
-    .select('id')
+    .select(`
+      email,
+      documents (
+          *
+      )
+  `)
     .eq('email', decodedToken.email)
     .single();
-
-  if (userError) {
-    console.error(userError);
-  }
-
-  const { data, error } = await supabase
-    .from('documents')
-    .select('*')
-    .eq('document_creator', user.id);
 
   if (error) {
     console.error(error);
