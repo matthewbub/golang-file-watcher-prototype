a4a0ac0e3375d023b74ab24c127a08b4fa31c9ab
matéo! <6matbub@gmail.com>
2023-08-03 03:31:15 -0700

    Cleanup Button

--- components/Button.jsx
+++ components/Button.jsx
@@ -9,25 +9,45 @@ import clsx from "clsx";
  * @param {...*} props.rest - Additional props for the button.
  * @returns {JSX.Element} The rendered Button component.
  */
-const Button = ({ children, className, styleType = 'primary', ...rest }) => {
+const Button = ({
+  children,
+  className,
+  styleType = 'primary',
+  disabled = false,
+  ...rest
+}) => {
   const styleTypes = {
-    primary: "bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 text-white",
-    secondary: "bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 text-white",
-    danger: "bg-red-600 hover:bg-red-700 focus:ring-red-500 text-white",
-    success: "bg-green-600 hover:bg-green-700 focus:ring-green-500 text-white",
-    warning: "bg-yellow-600 hover:bg-yellow-700 focus:ring-yellow-500 text-white",
-    info: "bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 text-white",
-    light: "bg-white hover:bg-gray-50 focus:ring-gray-500 text-gray-700",
-    dark: "bg-gray-900 hover:bg-gray-800 focus:ring-gray-500 text-white",
+    primary: "bg-indigo-600 text-white",
+    secondary: "bg-gray-600 text-white",
+    danger: "bg-red-600 text-white",
+    success: "bg-green-600 text-white",
+    warning: "bg-yellow-600 text-white",
+    info: "bg-blue-600 text-white",
+    light: "bg-white text-gray-700",
+    dark: "bg-gray-900 text-white",
   };
+
+  const stateStyles = {
+    primary: "hover:bg-indigo-700 focus:ring-indigo-500",
+    secondary: "hover:bg-gray-700 focus:ring-gray-500",
+    danger: "hover:bg-red-700 focus:ring-red-500",
+    success: "hover:bg-green-700 focus:ring-green-500",
+    warning: "hover:bg-yellow-700 focus:ring-yellow-500",
+    info: "hover:bg-blue-700 focus:ring-blue-500",
+    light: "hover:bg-gray-50 focus:ring-gray-500",
+    dark: "hover:bg-gray-800 focus:ring-gray-500",
+  };
+
+  const baseStyles = "inline-flex items-center text-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm"
 
   return (
     <button
       {...rest}
       className={clsx(
+        disabled ? "opacity-50 cursor-not-allowed" : stateStyles[styleType],
+        baseStyles,
         styleTypes[styleType],
-        "inline-flex items-center text-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2",
-        className,
+        className
       )}
     >
       {children}
