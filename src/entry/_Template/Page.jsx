04e69fbee91e35e81d05cbb86dddc0f27648751c
matéo! <6matbub@gmail.com>
2023-08-06 09:32:52 -0700

    Create Template Entry

---
+++ src/entry/_Template/Page.jsx
@@ -0,0 +1,16 @@
+import { ConsoleLayout } from '../../components';
+import InitialClientEx from "./InitialClientEx";
+
+
+const Page = ({ consoleLayout = {
+  title: "Page",
+  description: "Page description"
+} }) => {
+  return (
+    <InitialClientEx>
+      <ConsoleLayout {...consoleLayout} />
+    </InitialClientEx>
+  )
+}
+
+export default Page;
