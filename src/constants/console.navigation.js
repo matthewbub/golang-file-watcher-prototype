10ff3c37f0ee2e80719cd34e49a018d457d962e1
matéo! <6matbub@gmail.com>
2023-08-10 04:30:48 -0700

    Simplify navigation

--- src/constants/console.navigation.js
+++ src/constants/console.navigation.js
@@ -1,72 +1,8 @@
 export const consoleNavigation = [
   {
-    name: 'Administration',
+    name: 'Apps',
     type: 'section',
-    description: 'Internal administration pages for the console application',
-  },
-  {
-    name: 'Roles & Permissions',
-    href: '/users',
-    type: 'link',
-    description: 'User management services and pages for the console application',
-  },
-  {
-    name: 'Localization (i18n)',
-    href: '/messages',
-    type: 'link',
-    description: 'Internal messages management services and pages for the console application',
-  },
-  {
-    name: 'Logs',
-    href: '/logs',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Security',
-    href: '/security',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Screen Manager',
-    href: '/screen-manager',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'My Clients',
-    href: '/my-clients',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'My Team',
-    href: '/my-clients',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Experimental Features',
-    href: '/experimental',
-    type: 'link',
-    description: 'Experimental features and services.',
-    children: [
-      {
-        name: 'Endless Checkboxes',
-        href: '/experimental/endless-checkboxes',
-        description: 'Tenant management services and pages for the console application',
-      },
-      {
-        name: 'Checkbox Unique Conditions',
-        href: '/experimental/endless-checkboxes-unique-conditions',
-      },
-    ],
-  },
-  {
-    name: 'Internal Apps',
-    type: 'section',
-    description: 'External api services used within the console application for tenant management and deployment management',
+    description: 'Internal application pages for the console application',
   },
   {
     name: 'Documents',
@@ -81,13 +17,7 @@ export const consoleNavigation = [
     description: 'Coming soon...',
   },
   {
-    name: 'Messages',
-    href: '/direct-messages',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Time Logger',
+    name: 'Project Manager',
     href: '/experimental/time-logger',
     type: 'link',
     description: 'Securely track your time spent on projects.',
@@ -98,59 +28,34 @@ export const consoleNavigation = [
     type: 'link',
     description: 'Coming soon...',
   },
-  {
-    name: 'Help Desk',
-    href: '/help-desk',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Forms',
-    href: '/forms',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'Analytics',
-    href: '/forms',
-    type: 'link',
-    description: 'Coming soon...',
-  },
-  {
-    name: 'External Services',
-    type: 'section',
-    description: 'External api services used within the console application for tenant management and deployment management',
-  },
-  {
-    name: 'Hosting',
-    href: '/hosting',
-    type: 'link',
-    description: 'Git history. Vercel hosting services and deployments. Also includes domain management, tenant management and deployment management.',
-    children: [
-      {
-        name: 'Tenants',
-        href: '/hosting/tenants',
-        description: 'Tenant management services and pages for the console application',
-      },
-      {
-        name: 'Configured Domains',
-        href: '/hosting/domains',
-        description: 'Domain management services and pages for the console application',
-      },
-    ],
-  },
-  {
-    name: 'Recent Deployments',
-    type: 'link',
-    href: '/hosting/deployments',
-    description: 'Deployment management services and pages for the console application',
-  },
-  {
-    name: '3rd Party Services',
-    type: 'link',
-    href: '/3rd-party-services',
-    description: 'Coming soon...',
-  },
-
-
+  //   {
+  //     name: 'Administrative',
+  //     href: '/hosting',
+  //     type: 'link',
+  //     description: 'Git history. Vercel hosting services and deployments. Also includes domain management, tenant management and deployment management.',
+  //     children: [
+  //       {
+  //         name: 'Tenants',
+  //         href: '/hosting/tenants',
+  //         description: 'Tenant management services and pages for the console application',
+  //       },
+  //       {
+  //         name: 'Configured Domains',
+  //         href: '/hosting/domains',
+  //         description: 'Domain management services and pages for the console application',
+  //       },
+  //       {
+  //         name: 'Logs',
+  //         href: '/logs',
+  //         type: 'link',
+  //         description: 'Coming soon...',
+  //       },
+  //       {
+  //         name: 'Recent Deployments',
+  //         type: 'link',
+  //         href: '/hosting/deployments',
+  //         description: 'Deployment management services and pages for the console application',
+  //       },
+  //   ],
+  // }
 ]
