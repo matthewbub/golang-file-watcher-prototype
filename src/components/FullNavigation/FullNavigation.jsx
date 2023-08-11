3953625fff29c89def5a179c537b0ef9f13edcb8
matéo! <6matbub@gmail.com>
2023-08-11 09:18:57 -0700

    Reintroduce icons to FullNavigation

--- src/components/FullNavigation/FullNavigation.jsx
+++ src/components/FullNavigation/FullNavigation.jsx
@@ -1,54 +1,57 @@
 import clsx from 'clsx';
-import { baseClassNames } from '../../helpers/constants';
+import { StarIcon } from '@heroicons/react/24/outline';
+import { Fragment } from 'react';
 
 const FullNavigation = ({ navigation = [] }) => {
   return (
     <div className={clsx(
-      baseClassNames.containerPadding,
-      "overflow-hidden shadow sm:grid sm:grid-cols-2 sm:gap-4 space-y-4 sm:space-y-0"
+      'container-padding',
+      'overflow-hidden shadow',
+      'space-y-4 sm:space-y-0',
+      'sm:grid sm:grid-cols-2 sm:gap-4'
     )}>
       {navigation && navigation.length > 0 && navigation.map((action) => {
         if (action.type === 'section') return (
           null
         )
         return (
-          <div
+          <section
             key={action.name}
             className={clsx(
-              baseClassNames.secondaryBackground,
-              baseClassNames.secondaryBackgroundHover,
-              'rounded',
+              'bg2',
+              'border border-transparent rounded',
+              'hover:bg-transparent hover:border-teal-700',
               'group relative p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-teal-500'
             )}
           >
-            <div className="">
-              <h3 className={clsx(
-                baseClassNames.text,
-                "text-base font-semibold leading-6"
-              )}>
+            <Fragment>
+              <div className="h-16 w-16 flex items-center justify-center border border-neutral-700 group-hover:border-teal-700 rounded mb-5">
+                <action.icon
+                  className={clsx(
+                    'h-8 w-8 txt2',
+                    'group-hover:text-teal-500'
+                  )}
+                />
+              </div>
+              <h3 className={clsx("txt1 text-base font-semibold leading-6")}>
                 <a href={action.href} className="focus:outline-none">
-                  {/* Extend touch target to entire panel */}
                   <span className="absolute inset-0" aria-hidden="true" />
                   {action.name}
                 </a>
               </h3>
-              <p className={clsx(
-                "mt-2 text-sm",
-                baseClassNames.secondaryText
-              )}>
+              <p className={clsx("mt-2 text-sm txt2")}>
                 {action.description}
               </p>
-            </div>
+            </Fragment>
+
             <span
-              className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+              className="absolute right-6 top-6 txt2"
               aria-hidden="true"
             >
-              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-              </svg>
+              <StarIcon className='h-6 w-6 hover:fill-yellow-500 hover:text-yellow-500 cursor-pointer' />
             </span>
 
-          </div>
+          </section>
         )
       })}
     </div>
--- src/entry/ViewDocuments/components/TableRowEditableFields.jsx
+++ src/entry/ViewDocuments/components/TableRowEditableFields.jsx
@@ -27,7 +27,7 @@ const ModifyDocumentForm = () => {
     <MultiColumnFormWrapper
       title='Modify Document'
       description='Select any field to modify it.'
-      className='mb-10'
+      className='mb-10 pt-5'
       headingSize='small'
     >
       <form className='col-span-2'>
@@ -114,12 +114,34 @@ const DeleteDocumentForm = () => {
   )
 }
 const TableRowEditableFields = () => {
+  const tabs = [
+    { name: 'Stats', href: '#', current: true },
+    { name: 'Edit', href: '#', current: false },
+  ]
   return (
     <div className={clsx(
       'w-full',
       'border-t border-white/20',
       'divide-solid divide-y divide-white/20'
     )}>
+
+      <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 mt-5 sm:px-6 md:grid-cols-3 lg:px-8">
+        <nav className="flex space-x-4" aria-label="Tabs">
+          {tabs.map((tab) => (
+            <a
+              key={tab.name}
+              href={tab.href}
+              className={clsx(
+                tab.current ? 'bg-teal-100/50 text-white outline outline-teal-500' : 'text-gray-500 hover:text-gray-700',
+                'px-3 py-0.5 text-sm font-medium'
+              )}
+              aria-current={tab.current ? 'page' : undefined}
+            >
+              {tab.name}
+            </a>
+          ))}
+        </nav>
+      </div>
       <ModifyDocumentForm />
       <ModifySEOForm />
       <DeleteDocumentForm />
