8f1ed76afc2d03713963a32b23483353bf980857
matéo! <6matbub@gmail.com>
2023-08-18 07:27:03 -0700

    add pw type toggle

--- src/pages/console/sign-up.jsx
+++ src/pages/console/sign-up.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import { Button } from '../../components'
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
 
 export default function Page() {
   const { register, handleSubmit, formState: { errors }, setError } = useForm()
   const router = useRouter()
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
   const submitForm = async (data) => {
     const { email, password, confirmPassword } = data;
@@ -75,15 +80,21 @@ export default function Page() {
                 Password
               </label>
             </div>
-            <div className="mt-2">
+            <div className="mt-2 relative">
               <input
                 id="password"
-                name="password"
-                type="password"
+                name="password"                
+                type={showPassword ? "text" : "password"}
                 autoComplete="current-password"
                 {...register("password", { required: "This field is required" })}
                 className="block w-full rounded-md border-0  py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
               />
+              <div className="absolute inset-y-0 right-0 pr-3 pb-3 flex items-center cursor-pointer">
+                {showPassword ? 
+                  <EyeSlashIcon onClick={() => setShowPassword(false)} className='h-5 w-5 text-neutral-900'/> : 
+                  <EyeIcon onClick={() => setShowPassword(true)} className='h-5 w-5 text-neutral-900' />
+                }
+              </div>
               <div className='h-3'>
                 {errors?.password?.message && <p className="text-sm text-red-600">{errors?.password?.message}</p>}
               </div>
@@ -96,14 +107,26 @@ export default function Page() {
                 Confirm Password
               </label>
             </div>
-            <div className="mt-2">
+            <div className="relative mt-2">              
               <input
                 id="confirmPassword"
                 name="confirmPassword"
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 {...register("confirmPassword", { required: "This field is required" })}
                 className="text-black block w-full rounded-md border-0  py-1.5 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-teal-500 sm:text-sm sm:leading-6"
               />
+              <div className="absolute inset-y-0 right-0 pr-3 pb-3 flex items-center cursor-pointer">
+                {showConfirmPassword ? 
+                  <EyeSlashIcon 
+                    title='Hide Password'
+                    onClick={() => setShowConfirmPassword(false)} className='h-5 w-5 text-neutral-900'
+                  /> : 
+                  <EyeIcon 
+                    title='Show Password'
+                    onClick={() => setShowConfirmPassword(true)} className='h-5 w-5 text-neutral-900' 
+                  />
+                }
+              </div>
               <div className='h-3'>
                 {errors?.confirmPassword?.message && <p className="text-sm text-red-600">{errors?.confirmPassword?.message}</p>}
               </div>
