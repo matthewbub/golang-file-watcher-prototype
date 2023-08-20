69aee6765de011f1b0f44c8fff8b128eb3a96838
matéo! <6matbub@gmail.com>
2023-08-20 04:38:09 -0700

    assign var names to values in morgan setup

--- shell/base.js
+++ shell/base.js
@@ -1,10 +1,10 @@
 const baseVariables = {
-  setupExpressRateLimit_rateLimitWindowMs: 15 * 60 * 1000,
-  setupExpressRateLimit_maxRequestsPerWindow: 100,
-  setupExpressSlowDown_slowDownWindowMs: 15 * 60 * 1000,
-  setupExpressSlowDown_delayAfterRequests: 50,
-  setupExpressSlowDown_delayMilliseconds: 500,
-  
+  setupExpressRateLimit_rateLimitWindowMs: 15 * 60 * 1000, // 15 minutes
+  setupExpressRateLimit_maxRequestsPerWindow: 100, // 100 requests
+  setupExpressSlowDown_slowDownWindowMs: 15 * 60 * 1000, // 15 minutes
+  setupExpressSlowDown_delayAfterRequests: 50, // 50 requests
+  setupExpressSlowDown_delayMilliseconds: 500, // 0.5 seconds
+  setupMorgan_morganFormat: 'combined', // 'combined', 'common', 'dev', 'short', 'tiny'
 }
 
 module.exports = baseVariables
