5efb12b0f4d49a9edcb00eb711e7d3e325f12190
matéo! <6matbub@gmail.com>
2023-08-26 20:02:30 -0700

    watching files

--- core/content-watchman/main.go
+++ core/content-watchman/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 var relativePathToContent string = "../../content"
@@ -26,7 +28,41 @@ func readCurrentDir() string {
 }
 
 func main() {
-	var hello = readCurrentDir()
-	fmt.Println(hello)
+	var contentDir = readCurrentDir()
+	fmt.Println("Watching directory: ", contentDir)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				fmt.Println("Event: ", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fmt.Println("Modified file: ", event.Name)
+				}
+			case err := <-watcher.Errors:
+				fmt.Println("Error: ", err)
+			}
+		}
+	}()
+
+	// Add directory to watcher
+	err = watcher.Add(contentDir)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	<-done
 
 }
