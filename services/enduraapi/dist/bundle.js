ffdadc650f9a26b2bd8cc42c9c5fddbb8e519e33
9mbs <6matbub@gmail.com>
2023-09-14 04:47:35 -0700

    [services/enduraapi] fix - rm unsecure packages

--- services/enduraapi/dist/bundle.js
+++ services/enduraapi/dist/bundle.js
@@ -6,12 +6,10 @@ const fastify = Fastify({
   logger: true
 });
 
-// Declare a route
 fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' });
 });
 
-// Run the server!
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) throw err
   console.log(`Server is now listening on ${address}`);
--- services/enduraapi/rollup.config.mjs
+++ services/enduraapi/rollup.config.mjs
@@ -1,7 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
 
 export default {
   input: 'src/index.js',
@@ -13,9 +11,7 @@ export default {
     resolve({
       preferBuiltins: true
     }),
-    commonjs(),
-    builtins(),
-    globals()
+    commonjs()
   ],
   external: ['fastify']
 };
