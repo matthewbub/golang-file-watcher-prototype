f2d60bb46ca4b48eb164c0e90684feb54eb5ad61
9mbs <6matbub@gmail.com>
2023-09-15 06:30:03 -0700

    [services/enduraapi] feat - layout todo routes

--- services/enduraapi/src/index.js
+++ services/enduraapi/src/index.js
@@ -1,3 +1,9 @@
+// - HTTP Methods: Determine which HTTP methods (GET, POST, PUT, DELETE) will be used for each endpoint.
+  // - `GET /todos` - Fetch all tasks
+  // - `GET /todos/:id` - Fetch a specific task
+  // - `POST /todos` - Create a new task
+  // - `PUT /todos/:id` - Update a task
+  // - `DELETE /todos/:id` - Delete a task
 import Fastify from 'fastify'
 
 const fastify = Fastify({
@@ -8,7 +14,27 @@ fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
 })
 
+fastify.get('/todos', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+fastify.get('/todos/:id', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+fastify.post('/todos', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+fastify.put('/todos/:id', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+fastify.delete('/todos/:id', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) throw err
-  console.log(`Server is now listening on ${address}`)
+  console.log(`Server is now listening on http://localhost:${address}`)
 })
